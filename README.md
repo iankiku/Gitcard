e2610875a7657c9a02b318b89d7d7d95fbb32453
Ian Kiku <iankiku@gmail.com>
2020-07-22 03:37:04 -0500

    api call with axios

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# gitcard
-Display github user basic info
+# GITCARD
+---
+Experimenting with React from ground up.
+
+##BUILDING SOMETHING COOL. 
+**When I get the free time to explore which is when I'm not working.**
+---
+
+### Things Todo!
+
+- [ ] Handle Errors
+- [ ] Modularize
+- [ ] Style
+- [ ] Keep coding
+
--- src/App.js
+++ src/App.js
@@ -1,81 +1,14 @@
 import React from 'react';
 import './App.css';
+import { Button, Container, AppBar, Toolbar, Typography } from '@material-ui/core';
+import  axios from 'axios'
 // //.. api data source https://api.github.com/users
 
-const testdata = [
-  {
-      "login": "mojombo",
-      "id": 1,
-      "node_id": "MDQ6VXNlcjE=",
-      "avatar_url": "https://avatars0.githubusercontent.com/u/1?v=4",
-      "gravatar_id": "",
-      "url": "https://api.github.com/users/mojombo",
-      "html_url": "https://github.com/mojombo",
-      "followers_url": "https://api.github.com/users/mojombo/followers",
-      "following_url": "https://api.github.com/users/mojombo/following{/other_user}",
-      "gists_url": "https://api.github.com/users/mojombo/gists{/gist_id}",
-      "starred_url": "https://api.github.com/users/mojombo/starred{/owner}{/repo}",
-      "subscriptions_url": "https://api.github.com/users/mojombo/subscriptions",
-      "organizations_url": "https://api.github.com/users/mojombo/orgs",
-      "repos_url": "https://api.github.com/users/mojombo/repos",
-      "events_url": "https://api.github.com/users/mojombo/events{/privacy}",
-      "received_events_url": "https://api.github.com/users/mojombo/received_events",
-      "type": "User",
-      "site_admin": false,
-      "name": "Tom Preston-Werner",
-      "company": null,
-      "blog": "http://tom.preston-werner.com",
-      "location": "San Francisco",
-      "email": null,
-      "hireable": null,
-      "bio": null,
-      "twitter_username": null,
-      "public_repos": 61,
-      "public_gists": 62,
-      "followers": 22033,
-      "following": 11,
-      "created_at": "2007-10-20T05:24:19Z",
-      "updated_at": "2020-06-24T19:57:44Z"
-    }, {
-      "login": "pjhyett",
-      "id": 3,
-      "node_id": "MDQ6VXNlcjM=",
-      "avatar_url": "https://avatars0.githubusercontent.com/u/3?v=4",
-      "gravatar_id": "",
-      "url": "https://api.github.com/users/pjhyett",
-      "html_url": "https://github.com/pjhyett",
-      "followers_url": "https://api.github.com/users/pjhyett/followers",
-      "following_url": "https://api.github.com/users/pjhyett/following{/other_user}",
-      "gists_url": "https://api.github.com/users/pjhyett/gists{/gist_id}",
-      "starred_url": "https://api.github.com/users/pjhyett/starred{/owner}{/repo}",
-      "subscriptions_url": "https://api.github.com/users/pjhyett/subscriptions",
-      "organizations_url": "https://api.github.com/users/pjhyett/orgs",
-      "repos_url": "https://api.github.com/users/pjhyett/repos",
-      "events_url": "https://api.github.com/users/pjhyett/events{/privacy}",
-      "received_events_url": "https://api.github.com/users/pjhyett/received_events",
-      "type": "User",
-      "site_admin": false,
-      "name": "PJ Hyett",
-      "company": "GitHub, Inc.",
-      "blog": "https://hyett.com",
-      "location": "San Francisco",
-      "email": null,
-      "hireable": null,
-      "bio": null,
-      "twitter_username": null,
-      "public_repos": 8,
-      "public_gists": 21,
-      "followers": 8215,
-      "following": 30,
-      "created_at": "2008-01-07T17:54:22Z",
-      "updated_at": "2020-05-08T03:10:49Z"
-    }]
 
 //function component
-const CardList =(props) =>(
+const CardList = (props) => (
   <div>
-    <Card {...testdata[0]}/>
-    <Card {...testdata[1]}/>
+    {props.profiles.map(profile => <Card {...profile} key={profile.id+1} />)}
   </div>
 )
 
@@ -88,22 +21,55 @@ class Card extends React.Component {
 
 
     return (
-    <div className="git-profile">
-      <img src={profile.avatar_url} alt="avatar"/>
-      <div className="info">
-        <div className="name">{profile.name} </div>
-        <div className="name">{profile.email} </div>
-        <div className="name">{profile.bio} </div>
-        <div className="name">{profile.company} </div>
-        <div className="name">{profile.organizations_url} </div>
-        <div className="name">{profile.location} </div>
-        <div className="name">{profile.twitter_username} </div>
-        <div className="name">{profile.blog}</div>
-        <div className="name">{profile.hireable} </div>
-        <div className="name">{profile.followers} </div> 
-      </div>
-      </div>
-      );
+      <Container maxWidth="sm">
+        <div className="git-profile">
+          <img src={profile.avatar_url} alt="avatar" />
+          <div className="info">
+            <div className="name">{profile.name} </div>
+            <div className="item">{profile.email} </div>
+            <div className="item">{profile.bio} </div>
+            <div className="item">{profile.company} </div>
+            <div className="item">{profile.organizations_url} </div>
+            <div className="item">{profile.location} </div>
+            <div className="item">{profile.twitter_username} </div>
+            <div className="item">{profile.blog}</div>
+            <div className="item">{profile.hireable} </div>
+            <div className="item">{profile.followers} </div>
+          </div>
+        </div>
+      </Container>
+    );
+  }
+}
+// class component
+class Form extends React.Component {
+  // submit fnct
+  state = { userName: ''};
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    var url= "https://api.github.com/users/"
+    //get data from github with axios
+
+    const response = await axios.get(`${url}${this.state.userName}`)
+
+    this.props.onSubmit(response.data)
+    this.setState({ userName: '' })
+
+  };
+
+  render() {
+
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="Enter GitHub Username" 
+        //ref={this.userNameInput}  instead of using ref use a state object
+        value={this.state.userName}
+        onChange={event=>this.setState({ userName: event.target.value })}
+        required />
+        <button>Add Card</button>
+      </form>
+    );
   }
 }
 
@@ -111,14 +77,30 @@ class Card extends React.Component {
 
 
 class App extends React.Component {
-  // constructure
-  // this
 
+
+  state = {
+    profiles: [],
+  }
+
+  addNewProfile = (profileData) => {
+    this.setState(prevState => ({
+      profiles: [...prevState.profiles, profileData]
+    }))
+  } 
 
   render() {
     return (<div>
-      <div className="header">{this.props.title}</div>
-      <CardList/>
+
+      <AppBar position="static">
+        <Toolbar>
+          <div className="header">{this.props.title}</div>
+        </Toolbar>
+      </AppBar>
+
+      <Form onSubmit={this.addNewProfile}/>
+
+      <CardList profiles={this.state.profiles} />
     </div>);
   }
 }
